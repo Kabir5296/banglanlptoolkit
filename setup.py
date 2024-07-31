24337b7937fbe16fb2a3a74662d43e2fe0e43a54
A F M Mahfuzul Kabir <87871701+Kabir5296@users.noreply.github.com>
2024-07-31 10:21:37 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.1.8.dev0' 
+VERSION = '1.1.7.dev2' 
 DESCRIPTION = 'Toolkits for text processing and augmentation for Bangla NLP'
 
 REQUIREMENTS = [
